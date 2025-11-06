28d3792614113d93d0cb65ba9cf52e6969d428e5
inass090 <nena0009000@gmail.com>
2025-11-06 18:14:42 +0800

    completed Exercise3.cpp
    the user writes a number and  the program will tell if it's an odd or even number

--- Exercise3.cpp
+++ Exercise3.cpp
@@ -1,3 +1,33 @@
 //Title: Even or Odd
 //Description: Write a program that asks the user to enter a number and then determines whether the number is even or odd. 
 //Print an appropriate message to the console.
+#include<iostream>
+using namespace std;
+
+
+int main (){
+
+int unumber;
+cout<<"welcome, Enter a number and we will tell you if it's even or odd: ";
+cin>>unumber;
+
+if (unumber%2==0){
+
+cout<<endl<< unumber <<" is an even number"<<endl;
+
+}
+else{
+cout<< unumber<<" is an odd number";}
+
+
+
+  return 0;
+   }
+
+
+
+
+
+
+return 0;
+)
