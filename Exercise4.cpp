043cb5dcb2d2bb152af9bf4e727c5e006d2eae2b
inass090 <nena0009000@gmail.com>
2025-11-08 12:22:43 +0800

    completed Exercise4.cpp

--- Exercise4.cpp
+++ Exercise4.cpp
@@ -2,3 +2,33 @@
 //Description: Write a program that prints the Fibonacci sequence up to a given number. 
 //The Fibonacci sequence is a series of numbers in which each number is the sum of the two preceding ones. 
 //The sequence starts with 0 and 1.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	
+int first_numb=0;
+int second_numb=1;
+int next=0;
+int users_numb;
+cout<<"welcome to my Fibonacci series"<<endl<<"Enter a number and my wonderful program will add the numbers untel the number you entered" <<endl;
+cin>>users_numb;
+
+cout<<"Febonacci series for "<<users_numb<<" is "<<endl;
+
+while(next<users_numb){
+	
+	next=first_numb+second_numb;
+	cout<<" "<<next<<" ";
+	first_numb=second_numb;
+	second_numb=next;
+} 
+
+	
+		
+	
+	return 0;
+	
+}
